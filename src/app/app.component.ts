50d2a5816d9f701d8f089d4142ceb8f94ac014ff
parth <parthvin@gmail.com>
2020-05-16 01:55:07 +0530

    Added the audio card (still buggy with correct answers)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {
 } from "@angular/core";
 import { Location } from "@angular/common";
 import { DOCUMENT } from "@angular/common";
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: "app-root",
@@ -15,6 +16,7 @@ import { DOCUMENT } from "@angular/common";
 })
 export class AppComponent implements OnInit {
   constructor(
+    private router: Router,
     private renderer: Renderer2,
     public location: Location,
     @Inject(DOCUMENT) document
@@ -37,5 +39,11 @@ export class AppComponent implements OnInit {
   }
   ngOnInit() {
     this.onWindowScroll(event);
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
   }
 }
--- src/app/pages/audiopage/audiopage.component.ts
+++ src/app/pages/audiopage/audiopage.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 @Component({
   selector: 'app-audiopage',
   templateUrl: './audiopage.component.html',
-  styleUrls: ['./audiopage.component.scss']
+  styleUrls: [
+    './audiopage.component.scss',
+  ]
 })
 export class AudioPageComponent implements OnInit, OnDestroy {
 
@@ -11,7 +13,56 @@ export class AudioPageComponent implements OnInit, OnDestroy {
   focus;
   focus1;
   focus2;
-  constructor() { }
+
+  index: number;
+  correct_index: number;
+  audio_url: string;
+  name1: string;
+  name2: string;
+  name3: string;
+  index1: number;
+  index2: number;
+  index3: number;
+  currentState: boolean;
+
+  audioMessages = [
+    [
+      'Parth Patil',
+      'assets/audio/background_sound.mp3'
+    ],
+    [
+      'Madhura Pawar',
+      'assets/audio/background_sound.mp3'
+    ],
+    [
+      'Amruta Takle',
+      'assets/audio/background_sound.mp3'
+    ],
+    [
+      'Adhirath Salvi',
+      'assets/audio/background_sound.mp3'
+    ],
+    [
+      'Nahush Kumbhar',
+      'assets/audio/background_sound.mp3'
+    ],
+    [
+      'Pranita Achrekar',
+      'assets/audio/background_sound.mp3'
+    ],
+  ]
+
+  constructor() {
+    this.index = 0;
+    for (var i = this.audioMessages.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.audioMessages[i];
+      this.audioMessages[i] = this.audioMessages[j];
+      this.audioMessages[j] = temp;
+    }
+    this.selectRandomMessage();
+    this.currentState = true;
+   }
   @HostListener("document:mousemove", ["$event"])
   onMouseMove(e) {
     var squares1 = document.getElementById("square1");
@@ -85,5 +136,71 @@ export class AudioPageComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.remove("register-page");
+  }
+
+  checkAnswer(answer: number){
+    let answer_boxes = [
+      document.getElementById("answer1"),
+      document.getElementById("answer2"),
+      document.getElementById("answer3")
+    ]
+    for (let i = 0; i < 3; i++) {
+      if (i == this.index){
+        answer_boxes[i].classList.add("correct");
+      }
+      else{
+        answer_boxes[i].classList.add("wrong");
+      }
+    }
+    this.selectRandomMessage();
+    setTimeout(() => {
+      for (let i = 0; i < 3; i++) {
+          answer_boxes[i].classList.remove("correct");
+          answer_boxes[i].classList.remove("wrong");
+      }
+    }, 1000);
+  }
+
+  selectRandomMessage(direction=true){
+    console.log(this);
+    this.index = direction ? this.index + 1 : this.index - 1;
+    if (this.index >= this.audioMessages.length) this.index = 0;
+    if (this.index < 0) this.index = this.audioMessages.length - 1;
+
+    let taken = [this.index];
+    let random = [-1, -1, -1];
+    let loop_index = 0;
+    random[Math.floor(Math.random() * 3)] = this.index;
+    let loop_stop = 0;
+
+    while(loop_index < 3 && loop_stop < 100){
+      loop_stop++;
+      let rand_index = Math.floor(Math.random() * this.audioMessages.length);
+      // console.log(loop_index, rand_index, taken, random);
+      if (random[loop_index] >= 0) {
+        loop_index++;
+        continue;
+      }
+
+      let skip = false;
+      for (let j = 0; j < taken.length; j++) {
+        // console.log("in loop", (taken[j] == rand_index));
+        if (taken[j] == rand_index){
+          skip = true;
+          break;
+        }
+      }
+      if (skip) continue;
+
+      taken.push(rand_index);
+      random[loop_index] = rand_index;
+      loop_index++;
+    }
+    this.index1 = random[0];
+    this.index2 = random[1];
+    this.index3 = random[2];
+    this.name1 = this.audioMessages[this.index1][0];
+    this.name2 = this.audioMessages[this.index2][0];
+    this.name3 = this.audioMessages[this.index3][0];
   }
 }
